f2e34905697fd6f73b7dd7c5defda6a3b8803c07
joselo7990 <josemachicote@hotmail.com>
2023-09-09 17:38:33 -0300

    j

--- mi-proyecto/src/components/Header.jsx
+++ mi-proyecto/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = function () {
       <nav class="navbar navbar-expand-lg navbar-light bg-danger d-flex flex-row">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            flix
+            Hackflix
           </a>
         </div>
       </nav>
